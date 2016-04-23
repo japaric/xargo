bbad77e4a2d1f5bb77279d481b3286ee99271140
Jorge Aparicio <japaricious@gmail.com>
2016-04-23 18:22:03 -0500

    rewrite using the cargo library as a base
    improvements:
    
    - we now report errors to the console instead of paniking.
    - we now report Xargo current status to the console instead of logging it.
    - you can use Xargo concurrently thanks to file locking.
    
    refactors:
    
    - use Cargo's 'config' parsing support instead of rolling our own.
    - now that we are printing to the console, I have removed logging.
    
    closes #5
    closes #7

--- src/sysroot.rs
+++ src/sysroot.rs
@@ -1,192 +1,287 @@
-use std::env;
 use std::fs::{self, File};
-use std::io::{self, Read, Write};
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
-use std::process::{self, Command};
+use std::process::Command;
 
+use cargo::util::{self, CargoResult, ChainError, Config, Filesystem};
 use chrono::NaiveDate;
-use curl::http::Handle;
+use curl::http;
 use flate2::read::GzDecoder;
 use rustc_version::{self, Channel};
 use tar::Archive;
 use tempdir::TempDir;
+use term::color::GREEN;
 
 use Target;
 
-pub fn create(target: &Target) -> PathBuf {
-    let meta = rustc_version::version_meta();
+macro_rules! try {
+    ($e:expr) => {
+        match $e {
+            Ok(x) => x,
+            _ => panic!("{}", line!()),
+        }
+    }
+}
+
+/// Create a sysroot that looks like this:
+///
+/// ``` text
+/// ~/.xargo
+/// ├── date
+/// ├── lib
+/// │   └── rustlib
+/// │       ├── $HOST
+/// │       │   └── lib
+/// │       │       ├── libcore-$hash.rlib -> $SYSROOT/lib/rustlib/$HOST/lib/libcore-$hash.rlib
+/// │       │       └── (..)
+/// │       ├── $TARGET1
+/// │       │   ├── hash
+/// │       │   └── lib
+/// │       │       ├── libcore-$hash.rlib
+/// │       │       └── (..)
+/// │       ├── $TARGET2
+/// │       │   └── (..)
+/// │       ├── (..)
+/// │       └── $TARGETN
+/// │           └── (..)
+/// └── src
+///     │── libcore
+///     │── libstd
+///     └── (..)
+/// ```
+///
+/// Where:
+///
+/// - `$SYSROOT` is the current `rustc` sysroot i.e. `$(rustc --print sysroot)`
+/// - `$HOST` is the current `rustc`'s host i.e. the host field in `$(rustc -Vv)`
+/// - `$TARGET*` are the custom targets `xargo` is managing
+///
+/// The `~/.xargo` is mostly a "standard" sysroot but with extra information:
+///
+/// - the `hash` files which track changes in the `$TARGET`s' specification files (i.e.
+/// `$TARGET.json`).
+/// - the `src` directory which holds the source code of the current `rustc` (and standard crates).
+/// - the `date` file which holds the build date of the current `rustc`.
+pub fn create(config: &Config, target: &Target, root: &Filesystem) -> CargoResult<()> {
+    let meta = rustc_version::version_meta_for(&config.rustc_info().verbose_version);
 
     if meta.channel != Channel::Nightly {
-        panic!("Only the nightly channel is currently supported");
+        return Err(util::human("Only the nightly channel is currently supported"));
     }
 
-    let home = &env::home_dir().unwrap();
-    let commit_date = NaiveDate::parse_from_str(meta.commit_date.as_ref().unwrap(), "%Y-%m-%d")
-                          .unwrap();
-    // XXX AFAIK this is not guaranteed to be correct, but it appears to be a good approximation
+    let commit_date = try!(meta.commit_date
+                               .as_ref()
+                               .and_then(|s| NaiveDate::parse_from_str(s, "%Y-%m-%d").ok())
+                               .ok_or(util::human("couldn't find/parse the commit date from \
+                                                   `rustc -Vv`")));
+    // XXX AFAIK this is not guaranteed to be correct, but it appears to be a good approximation.
     let build_date = commit_date.succ();
-    let build_date_ = &build_date.format("%Y-%m-%d").to_string();
-    info!("rustc is a nightly from: {}", build_date_);
 
-    let root = Path::new(home).join(".xargo");
-    let source_date = read_date(&root).ok();
+    try!(update_source(config, &build_date, root));
+    try!(rebuild_sysroot(config, root, target));
+    try!(symlink_host_crates(config, root));
 
-    if let Some(date) = source_date {
-        info!("cached nightly source is from: {}", date);
+    Ok(())
+}
+
+fn update_source(config: &Config, date: &NaiveDate, root: &Filesystem) -> CargoResult<()> {
+    const TARBALL: &'static str = "rustc-nightly-src.tar.gz";
+
+    /// Reads the `NaiveDate` stored in `~/.xargo/date`
+    fn read_date(root: &Path) -> CargoResult<Option<NaiveDate>> {
+        let date = &mut String::new();
+        try!(try!(File::open(root.join("date"))).read_to_string(date));
+
+        Ok(NaiveDate::parse_from_str(date, "%Y-%m-%d").ok())
     }
 
-    if source_date != Some(build_date) {
-        fetch_source(&root, build_date_);
-    }
+    fn download(config: &Config, date: &NaiveDate) -> CargoResult<http::Response> {
+        const B_PER_S: usize = 1;
+        const MS: usize = 1_000;
+        const S: usize = 1;
 
-    let prev_hash = read_hash(&root, target).ok();
-    if prev_hash != Some(target.hash) {
-        if prev_hash.is_some() {
-            info!("target specification file hash changed");
+        // NOTE Got these settings from cargo (src/cargo/ops/registry.rs)
+        let mut handle = http::handle()
+                             .timeout(0)
+                             .connect_timeout(30 * MS)
+                             .low_speed_limit(10 * B_PER_S)
+                             .low_speed_timeout(30 * S);;
+
+        let url = format!("https://static.rust-lang.org/dist/{}/{}",
+                          date.format("%Y-%m-%d"),
+                          TARBALL);
+
+        try!(config.shell().out().say_status("Downloading", &url, GREEN, true));
+        let resp = try!(handle.get(url).follow_redirects(true).exec());
+
+        let code = resp.get_code();
+        if code != 200 {
+            return Err(util::human(format!("HTTP error got {}, expected 200", code)));
         }
 
-        build_crates(&root, target);
+        Ok(resp)
     }
 
-    if !root.join(format!("lib/rustlib/{}", meta.host)).exists() {
-        symlink_host_crates(&meta.host, &root);
+    fn unpack(config: &Config, tarball: http::Response, root: &Path) -> CargoResult<()> {
+        try!(config.shell().out().say_status("Unpacking", TARBALL, GREEN, true));
+
+        let src_dir = &root.join("src");
+        try!(fs::create_dir(src_dir));
+
+        let decoder = try!(GzDecoder::new(tarball.get_body()));
+        let mut archive = Archive::new(decoder);
+        for entry in try!(archive.entries()) {
+            let mut entry = try!(entry);
+            let path = {
+                let path = try!(entry.path());
+                let mut components = path.components();
+                components.next();
+                let next = components.next().and_then(|s| s.as_os_str().to_str());
+                if next != Some("src") {
+                    continue;
+                }
+                components.as_path().to_path_buf()
+            };
+            try!(entry.unpack(src_dir.join(path)));
+        }
+
+        Ok(())
     }
 
-    root
-}
+    let lock = try!(root.open_rw("date", config, "xargo"));
 
-/// Reads `~/.xargo/date`
-fn read_date(xargo_dir: &Path) -> io::Result<NaiveDate> {
-    let date = &mut String::new();
-    File::open(xargo_dir.join("date"))
-        .and_then(|mut f| f.read_to_string(date))
-        .map(|_| try!(NaiveDate::parse_from_str(date, "%Y-%m-%d")))
-}
-
-/// Reads `~/.xargo/lib/rustlib/{target}/hash`
-fn read_hash(root: &Path, target: &Target) -> io::Result<u64> {
-    let hash = &mut String::new();
-    File::open(root.join(format!("lib/rustlib/{}/hash", target.triple)))
-        .and_then(|mut f| f.read_to_string(hash))
-        .map(|_| try!(hash.parse()))
-}
-
-/// Fetches nightly source from `date` into `$root/src`
-fn fetch_source(root: &Path, date: &str) {
-    if root.exists() {
-        info!("purging ~/.xargo");
-        try!(fs::remove_dir_all(root));
+    if try!(read_date(lock.parent())).as_ref() == Some(date) {
+        // Source is up to date
+        return Ok(());
     }
 
-    info!("fetching source tarball");
-    let handle = Handle::new();
-    let url = format!("https://static.rust-lang.org/dist/{}/rustc-nightly-src.tar.gz",
-                      date);
-    let resp = try!(handle.timeout(600_000).get(url).follow_redirects(true).exec());
+    try!(lock.remove_siblings());
+    let tarball = try!(download(config, date)
+                           .chain_error(|| util::human("Couldn't fetch Rust source tarball")));
+    try!(unpack(config, tarball, lock.parent())
+             .chain_error(|| util::human("Couldn't unpack Rust source tarball")));
 
-    assert_eq!(resp.get_code(), 200);
+    let mut file = lock.file();
+    try!(file.set_len(0));
+    try!(file.write_all(date.format("%Y-%m-%d").to_string().as_bytes()));
 
-    info!("unpacking source tarball");
-    let src_dir = &root.join("src");
-    try!(fs::create_dir_all(src_dir));
-
-    let decoder = try!(GzDecoder::new(resp.get_body()));
-    let mut archive = Archive::new(decoder);
-    for entry in try!(archive.entries()) {
-        let mut entry = try!(entry);
-        let path = {
-            let path = try!(entry.path());
-            let mut components = path.components();
-            components.next();
-            let next = components.next().and_then(|s| s.as_os_str().to_str());
-            if next != Some("src") {
-                continue;
-            }
-            components.as_path().to_path_buf()
-        };
-        try!(entry.unpack(src_dir.join(path)));
-    }
-    try!(try!(File::create(root.join("date"))).write_all(date.as_bytes()));
+    Ok(())
 }
 
-fn build_crates(root: &Path, target: &Target) {
-    // NOTE These crates pull all the other free-standing crates
+fn rebuild_sysroot(config: &Config, root: &Filesystem, target: &Target) -> CargoResult<()> {
+    /// Reads the hash stored in `~/.xargo/lib/rustlib/$TARGET/hash`
+    fn read_hash(mut file: &File) -> CargoResult<Option<u64>> {
+        let hash = &mut String::new();
+        try!(file.read_to_string(hash));
+        Ok(hash.parse().ok())
+    }
+
     const CRATES: &'static [&'static str] = &["collections", "rand"];
-    const CARGO_TOML: &'static str = "\
-[package]
+    const TOML: &'static str = "[package]
 name = 'sysroot'
 version = '0.0.0'
 
 [dependencies]
 ";
-    info!("cross compiling crates");
 
-    let temp_dir_ = try!(TempDir::new("xargo"));
-    let temp_dir = temp_dir_.path();
+    let outer_lock = try!(root.open_ro("date", config, "xargo"));
+    let lock = try!(root.open_rw(format!("lib/rustlib/{}/hash", target.triple),
+                                 config,
+                                 &format!("xargo/{}", target.triple)));
+    let root = outer_lock.parent();
+
+    if try!(read_hash(lock.file())) == Some(target.hash) {
+        // Target specification file unchanged
+        return Ok(());
+    }
+
+    let target_dir = lock.parent();
+    try!(config.shell().out().say_status("Compiling",
+                                         format!("sysroot for {}", target.triple),
+                                         GREEN,
+                                         true));
+
+    let td = try!(TempDir::new("xargo"));
+    let td = td.path();
 
     // Create Cargo project
-    try!(fs::create_dir(temp_dir.join("src")));
-    try!(fs::copy(&target.path,
-                  temp_dir.join(target.path.file_name().unwrap())));
-    try!(File::create(temp_dir.join("src/lib.rs")));
-    let toml = &mut String::from(CARGO_TOML);
+    try!(fs::create_dir(td.join("src")));
+    try!(fs::copy(&target.path, td.join(target.path.file_name().unwrap())));
+    try!(File::create(td.join("src/lib.rs")));
+    let toml = &mut String::from(TOML);
     for krate in CRATES {
         toml.push_str(&format!("{} = {{ path = '{}' }}\n",
                                krate,
-                               root.join(format!("src/lib{}", krate)).display()));
+                               root.join(format!("src/lib{}", krate)).display()))
     }
-    try!(try!(File::create(temp_dir.join("Cargo.toml"))).write_all(toml.as_bytes()));
+    try!(try!(File::create(td.join("Cargo.toml"))).write_all(toml.as_bytes()));
 
+    // Build Cargo project
     let cargo = &mut Command::new("cargo");
     cargo.args(&["build", "--release", "--target"]);
     cargo.arg(&target.triple);
     for krate in CRATES {
         cargo.args(&["-p", krate]);
     }
-    cargo.current_dir(temp_dir);
-    info!("calling: {:?}", cargo);
-    let output = try!(cargo.output());
+    cargo.current_dir(td);
+    let status = try!(cargo.status());
 
-    if output.status.success() {
-        info!("\n{}", String::from_utf8_lossy(&output.stdout));
-    } else {
-        info!("\n{}", String::from_utf8_lossy(&output.stdout));
-        error!("\n{}", String::from_utf8_lossy(&output.stderr));
-
-        process::exit(output.status.code().unwrap_or(1));
+    if !status.success() {
+        return Err(util::human("`cargo` process didn't exit successfully"));
     }
 
-    let dst_dir = &root.join(format!("lib/rustlib/{}/lib", target.triple));
-    try!(fs::create_dir_all(dst_dir));
-    for entry in try!(fs::read_dir(temp_dir.join(format!("target/{}/release/deps",
-                                                         target.triple)))) {
+    // Copy build artifacts
+    if target_dir.exists() {
+        try!(fs::remove_dir_all(target_dir));
+    }
+    let dst = &target_dir.join("lib");
+    try!(fs::create_dir_all(dst));
+    for entry in try!(fs::read_dir(td.join(format!("target/{}/release/deps", target.triple)))) {
         let src = &try!(entry).path();
-        try!(fs::copy(src, dst_dir.join(src.file_name().unwrap())));
+        try!(fs::copy(src, dst.join(src.file_name().unwrap())));
     }
 
-    try!(try!(File::create(root.join(format!("lib/rustlib/{}/hash", target.triple))))
-             .write_all(target.hash.to_string().as_bytes()));
+    let mut file = lock.file();
+    try!(file.set_len(0));
+    try!(file.write_all(target.hash.to_string().as_bytes()));
+
+    Ok(())
 }
 
-fn symlink_host_crates(host: &str, hash_dir: &Path) {
-    info!("symlinking host crates");
+fn symlink_host_crates(config: &Config, root: &Filesystem) -> CargoResult<()> {
+    let _outer_lock = try!(root.open_ro("date", config, "xargo"));
+    let host = &config.rustc_info().host;
+    let lock = try!(root.open_rw(format!("lib/rustlib/{}/sentinel", host), config, &format!("xargo/{}", host)));
+    let dst = &lock.parent().join("lib");
 
-    let sysroot = try!(String::from_utf8(try!(Command::new("rustc")
-                                                  .args(&["--print", "sysroot"])
-                                                  .output())
-                                             .stdout));
-    let src = &PathBuf::from(sysroot.trim_right()).join(format!("lib/rustlib/{}/lib", host));
-    let dst = hash_dir.join(format!("lib/rustlib/{}/lib", host));
-
-    link_dir(src, &dst);
-}
-
-fn link_dir(src_dir: &Path, dst_dir: &Path) {
-    try!(fs::create_dir_all(dst_dir));
-
-    for entry in try!(fs::read_dir(src_dir)) {
-        let path = &try!(entry).path();
-
-        try!(fs::hard_link(path, dst_dir.join(path.file_name().unwrap())));
+    if dst.exists() {
+        return Ok(());
     }
+
+    try!(fs::create_dir_all(dst));
+    let src = try!(sysroot()).join(format!("lib/rustlib/{}/lib", host));
+
+    for entry in try!(fs::read_dir(src)) {
+        let src = &try!(entry).path();
+
+        try!(fs::hard_link(src, dst.join(src.file_name().unwrap())));
+    }
+
+    Ok(())
+}
+
+fn sysroot() -> CargoResult<PathBuf> {
+    let mut sysroot = try!(String::from_utf8(try!(Command::new("rustc")
+                                                      .args(&["--print", "sysroot"])
+                                                      .output())
+                                                 .stdout)
+                               .map_err(|_| {
+                                   util::human("output of `rustc --print sysroot` is not UTF-8")
+                               }));
+
+    while sysroot.ends_with('\n') {
+        sysroot.pop();
+    }
+
+    Ok(PathBuf::from(sysroot))
 }

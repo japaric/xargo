190c81b5fb1d6b233bb3f4d043de79eb511b970f
Jorge Aparicio <japaricious@gmail.com>
2016-10-14 19:49:09 -0500

    revamp the CI infrastructure
    closes #52
    closes #61

---
+++ ci/run.sh
@@ -0,0 +1,23 @@
+set -ex
+
+test_mode() {
+    cargo build --target $TARGET
+
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+        cargo test --target $TARGET
+    fi
+}
+
+deploy_mode() {
+    cargo rustc --target $TARGET --release -- -C lto
+}
+
+run() {
+    if [ -z $TRAVIS_TAG ]; then
+        test_mode
+    else
+        deploy_mode
+    fi
+}
+
+run

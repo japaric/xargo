afb2b4ffc681f3be2204f2ac2d5eb1a1e67142e3
Jorge Aparicio <japaricious@gmail.com>
2016-10-14 21:35:40 -0500

    fix package.ps1

--- ci/package.ps1
+++ ci/package.ps1
@@ -1,5 +1,3 @@
-Set-PSDebug -Trace 1
-
 $SRC_DIR = $PWD.Path
 $STAGE = [System.Guid]::NewGuid().ToString()
 
@@ -8,7 +6,7 @@ New-Item -Type Directory -Name $STAGE
 Set-Location $STAGE
 
 $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip"
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\hello.exe" '.\'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\xargo.exe" '.\'
 7z a "$ZIP" *
 
 Push-AppveyorArtifact "$ZIP"

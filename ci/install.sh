fd3225c82ec1c6d184c27ba5532ad8ea414fe305
Jorge Aparicio <jorge@japaric.io>
2017-05-13 12:03:18 -0500

    fix ci/install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -7,8 +7,10 @@ main() {
     local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-gnu
+        sort=sort
     else
         target=x86_64-apple-darwin
+        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
 
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \

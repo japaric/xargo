e399fc940f0dfae4e0f1712350db3100de74de4a
Aaron Hill <aa1ronham@gmail.com>
2019-12-29 15:56:17 -0500

    Remove 'default-run' from Cargo.toml on CI

--- ci/script.sh
+++ ci/script.sh
@@ -3,8 +3,12 @@ set -euxo pipefail
 beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 
 main() {
+    # We test against Cargo versions that don't support 'default-run'
+    # As a workaround, we remove 'default-run' from the Cargo.toml
+    # on CI
+    sed -i 's/default-run = "xargo"//g' Cargo.toml
     cross build --target $TARGET --locked
-    cross run --target $TARGET -- -V
+    cross run --bin xargo --target $TARGET -- -V
 
     if beginswith nightly $TRAVIS_RUST_VERSION; then
         cargo test --features dev --target $TARGET
